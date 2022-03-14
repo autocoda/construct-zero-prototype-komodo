a87d27c4cd54bd1b072c1c329ad1fc9708b7d6e4
Cezar Popa <cezar.popa@komododigital.co.uk>
2022-03-14 09:18:10 +0000

    [PP-3]Update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.entry('main')
+      .add('./src/scss/main.scss')
+      .end();
+  },
+
+  css: {
+    extract: {
+      filename: '[name].css',
+      chunkFilename: 'css/[name].css'
+    },
+    sourceMap: true
+  },
 })
