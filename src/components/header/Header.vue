40df88d1ffaf8af1e212e0ee8e66ceab0102ac8d
Cezar Popa <cezar.popa@komododigital.co.uk>
2022-03-15 11:46:03 +0000

    [PP-23]Add site header component

---
+++ src/components/header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="container">
+        <router-link to="/" class="navbar-brand d-flex align-items-center">
+          <img class="mb-2" width="172" height="40" src="@/assets/images/logo.png" alt="Project Pay logo">
+        </router-link>
+      </div>
+    </div>
+  </header>
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'MainHeader',
+})
+</script>
