1ac9e599135534723eabe3b2dab146c70d60c79a
Cezar Popa <cezar.popa@komododigital.co.uk>
2022-03-30 15:20:18 +0100

    [PP-21]Handle empty/null step totals gracefully

--- src/components/form/calculator-steps/Summary.vue
+++ src/components/form/calculator-steps/Summary.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="card bg-white p-4">
     <div class="row">
-
       <div class="col-12 mb-4">
         <div class="row">
           <div class="col-7">
@@ -14,69 +13,81 @@
         </div>
       </div>
 
-      <div class="col-12">
-        <h2 class="fw-lighter text-uppercase step-heading">Materials</h2>
-      </div>
-      <div class="col-6">
+      <div class="col-12" v-if="materialsStepEmissions !== '--'">
         <div class="row">
-          <div class="col-6">Bricks</div>
-          <div class="fw-bold col-6">{{ bricksEmissions }} kg</div>
-          <div class="col-6">Mortar</div>
-          <div class="fw-bold col-6">{{ mortarEmissions }} kg</div>
-          <div class="col-6">Vehicles</div>
-          <div class="fw-bold col-6">{{ vehicleEmissions }} kg</div>
-        </div>
-      </div>
-      <div class="col-6">
-        <div class="row">
-          <div class="col-6">Total</div>
-          <div class="col-6 fw-bold text-end">{{ materialsStepEmissions }}tonnes</div>
-        </div>
-      </div>
-
-      <div class="col-12 my-3">
-        <hr>
-      </div>
-      <div class="col-12">
-        <h2 class="fw-lighter text-uppercase step-heading">Equipment</h2>
-      </div>
-      <div class="col-6">
-        <div class="row" v-for="(item, index) in equipmentDetailedEmissions" :key="index">
-          <div class="col-12" v-if="item.completed === true">
+          <div class="col-12">
+            <h2 class="fw-lighter text-uppercase step-heading">Materials</h2>
+          </div>
+          <div class="col-6">
             <div class="row">
-              <div class="col-6">{{ item.equipmentName }}</div>
-              <div class="fw-bold col-6">{{ item.emissions.toFixed(4) }} kg</div>
+              <div class="col-6">Bricks</div>
+              <div class="fw-bold col-6">{{ bricksEmissions }} kg</div>
+              <div class="col-6">Mortar</div>
+              <div class="fw-bold col-6">{{ mortarEmissions }} kg</div>
+              <div class="col-6">Vehicles</div>
+              <div class="fw-bold col-6">{{ vehicleEmissions }} kg</div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="row">
+              <div class="col-6">Total</div>
+              <div class="col-6 fw-bold text-end">{{ materialsStepEmissions }}tonnes</div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-6">
-        <div class="row">
-          <div class="col-6">Total</div>
-          <div class="col-6 fw-bold text-end">{{ equipmentStepEmissions.toFixed(4) }} tonnes</div>
-        </div>
-      </div>
 
-      <div class="col-12 my-3">
-        <hr>
-      </div>
-      <div class="col-12">
-        <h2 class="fw-lighter text-uppercase step-heading">Personnel</h2>
-      </div>
-      <div class="col-6">
-        <div class="row" v-for="(item, index) in personnelDetailedEmissions" :key="index">
-          <div class="col-12" v-if="item.completed">
+      <div class="col-12" v-if="equipmentStepEmissions">
+        <div class="row">
+          <div class="col-12 my-3">
+            <hr>
+          </div>
+          <div class="col-12">
+            <h2 class="fw-lighter text-uppercase step-heading">Equipment</h2>
+          </div>
+          <div class="col-6">
+            <div class="row" v-for="(item, index) in equipmentDetailedEmissions" :key="index">
+              <div class="col-12" v-if="item.completed === true">
+                <div class="row">
+                  <div class="col-6">{{ item.equipmentName }}</div>
+                  <div class="fw-bold col-6">{{ item.emissions.toFixed(4) }} kg</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
             <div class="row">
-              <div class="col-6">Transport</div>
-              <div class="fw-bold col-6">{{ item.emissions.toFixed(2) }} kg</div>
+              <div class="col-6">Total</div>
+              <div class="col-6 fw-bold text-end">{{ equipmentStepEmissions.toFixed(4) }} tonnes</div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-6">
+
+      <div class="col-12" v-if="personnelStepEmissions">
         <div class="row">
-          <div class="col-6">Total</div>
-          <div class="col-6 fw-bold text-end">{{ personnelStepEmissions.toFixed(2) }} tonnes</div>
+          <div class="col-12 my-3">
+            <hr>
+          </div>
+          <div class="col-12">
+            <h2 class="fw-lighter text-uppercase step-heading">Personnel</h2>
+          </div>
+          <div class="col-6">
+            <div class="row" v-for="(item, index) in personnelDetailedEmissions" :key="index">
+              <div class="col-12" v-if="item.completed">
+                <div class="row">
+                  <div class="col-6">Transport</div>
+                  <div class="fw-bold col-6">{{ item.emissions.toFixed(2) }} kg</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="row">
+              <div class="col-6">Total</div>
+              <div class="col-6 fw-bold text-end">{{ personnelStepEmissions.toFixed(2) }} tonnes</div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -85,9 +96,9 @@
       </div>
 
       <div class="col-6">
-        <div class="align-items-baseline py-3 d-flex flex-row">
+        <div class="align-items-baseline d-flex flex-row">
           <img class="me-1" width="12" height="12" src="@/assets/images/calculator/steps/info.svg" alt="Info Icon Image">
-          You can return to any of the previous steps and enter more thorough information for a more accurate total before confirming.
+          <p>You can return to any of the previous steps and enter more thorough information for a more accurate total before confirming.</p>
         </div>
       </div>
 
@@ -134,7 +145,10 @@ export default defineComponent({
       return this.$store.getters.getPersonnelStepEmissions
     },
     summaryStepTotals: function () {
-      return parseFloat((this.materialsStepEmissions + this.equipmentStepEmissions + this.personnelStepEmissions).toFixed(2));
+      if (this.materialsStepEmissions && this.equipmentStepEmissions && this.personnelStepEmissions) {
+        return parseFloat((this.materialsStepEmissions + this.equipmentStepEmissions + this.personnelStepEmissions).toFixed(2));
+      }
+      return '--'
     }
   }
 })
