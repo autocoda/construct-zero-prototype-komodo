e9776c80165ef80ea8ccd4298be0530af800862c
Cezar Popa <cezar.popa@komododigital.co.uk>
2022-03-31 12:37:16 +0100

    [PP-5]Perform a route param reset via push on app reset

--- src/components/navigation/FormTopNavigation.vue
+++ src/components/navigation/FormTopNavigation.vue
@@ -81,6 +81,7 @@ export default defineComponent({
     },
     resetApplicationState: function () {
       this.$store.dispatch('resetState');
+      this.$router.push({name: 'home'});
     }
   },
 })
