702c251225f66fccc080a55a72f576b7ed5d6744
Cezar Popa <cezar.popa@komododigital.co.uk>
2022-03-31 10:51:36 +0100

    [PP-22]Add Application footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <MainHeader />
-  <div class="container">
+  <div class="container main-container">
     <router-view />
   </div>
+  <MainFooter />
 </template>
 <script>
 import MainHeader from "@/components/header/Header";
+import MainFooter from "@/components/footer/Footer";
 
 export default {
-  components: {MainHeader}
+  components: {MainHeader, MainFooter}
 }
 </script>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="bg-black py-5 mt-5"></footer>
+</template>
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'MainFooter'
+})
+</script>
